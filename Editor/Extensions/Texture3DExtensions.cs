f93c78fbbf56e77d201addf1ccdcdce623580130
Aler Denisov <aler.zampillo@gmail.com>
2019-03-09 03:26:02 +0800

    initial

---
+++ Editor/Extensions/Texture3DExtensions.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Extensions for Texture3D object so we can just invoke functions on them
+/// </summary>
+public static class Texture3DExtensions
+{
+    #region Members
+    /// <summary>
+    /// The material used to render the Texture3D
+    /// </summary>
+    private static Material _previewTexture3dMaterial;
+    /// <summary>
+    /// The texture that will be used for the background
+    /// </summary>
+    private static Texture2D _backgroundTexture;
+    /// <summary>
+    /// The GUIStyle that will be used for the background
+    /// </summary>
+    private static GUIStyle _backgroundGuiStyle;
+    #endregion
+
+    #region Functions
+    /// <summary>
+    /// Accessor to the material used to render the Texture3D
+    /// </summary>
+    private static Material PreviewTexture3dMaterial
+    {
+        get
+        {
+            if (_previewTexture3dMaterial == null)
+            {
+                _previewTexture3dMaterial = new Material(Shader.Find("Hidden/Texture3DPreview"));
+            }
+
+            return _previewTexture3dMaterial;
+        }
+    }
+
+    /// <summary>
+    /// Accessor to the static background texture
+    /// </summary>
+    public static Texture2D BackgroundTexture
+    {
+        get
+        {
+            if (_backgroundTexture == null)
+            {
+                _backgroundTexture = new Texture2D(1, 1);
+                _backgroundTexture.SetPixel(0, 0, Color.gray * 0.5f);
+                _backgroundTexture.Apply();
+            }
+
+            return _backgroundTexture;
+        }
+    }
+
+    /// <summary>
+    /// Accessor to the static background GUIStyle
+    /// </summary>
+    public static GUIStyle BackgroundGuiStyle
+    {
+        get
+        {
+            _backgroundGuiStyle = new GUIStyle();
+            _backgroundGuiStyle.active.background = BackgroundTexture;
+            _backgroundGuiStyle.focused.background = BackgroundTexture;
+            _backgroundGuiStyle.hover.background = BackgroundTexture;
+            _backgroundGuiStyle.normal.background = BackgroundTexture;
+
+            return _backgroundGuiStyle;
+        }
+    }
+
+    /// <summary>
+    /// Sets the parameters to the PreviewRenderUtility and calls the rendering
+    /// </summary>
+    /// <param name="texture3D">The Texture3D to preview</param>
+    /// <param name="angle">The camera angle</param>
+    /// <param name="distance">The distance of the camera to the preview cube</param>
+    /// <param name="samplingIterations">The amount of slices used to raymarch in the Texture3D</param>
+    /// <param name="density">A linear factor to multiply the Texture3D with</param>
+    private static void RenderInPreviewRenderUtility(Texture3D texture3D, Vector2 angle, float distance, int samplingIterations, float density)
+    {
+        PreviewTexture3dMaterial.SetInt("_SamplingQuality", samplingIterations);
+        PreviewTexture3dMaterial.SetTexture("_MainTex", texture3D);
+        PreviewTexture3dMaterial.SetFloat("_Density", density);
+
+        PreviewRenderUtilityHelpers.Instance.DrawMesh(MeshHelpers.Cube, Matrix4x4.identity, PreviewTexture3dMaterial, 0);
+
+        PreviewRenderUtilityHelpers.Instance.camera.transform.position = Vector2.zero;
+        PreviewRenderUtilityHelpers.Instance.camera.transform.rotation = Quaternion.Euler(new Vector3(-angle.y, -angle.x, 0));
+        PreviewRenderUtilityHelpers.Instance.camera.transform.position = PreviewRenderUtilityHelpers.Instance.camera.transform.forward * -distance;
+        PreviewRenderUtilityHelpers.Instance.camera.Render();
+    }
+
+    /// <summary>
+    /// Renders a preview of the Texture3D
+    /// </summary>
+    /// <param name="texture3D">The Texture3D to preview</param>
+    /// <param name="rect">The area where the preview is located</param>
+    /// <param name="angle">The camera angle</param>
+    /// <param name="distance">The distance of the camera to the preview cube</param>
+    /// <param name="samplingIterations">The amount of slices used to raymarch in the Texture3D</param>
+    /// <param name="density">A linear factor to multiply the Texture3D with</param>
+    /// <returns>A Texture with the preview</returns>
+    public static Texture RenderTexture3DPreview(this Texture3D texture3D, Rect rect, Vector2 angle, float distance, int samplingIterations, float density)
+    {
+        PreviewRenderUtilityHelpers.Instance.BeginPreview(rect, BackgroundGuiStyle);
+
+        RenderInPreviewRenderUtility(texture3D, angle, distance, samplingIterations, density);
+
+        return PreviewRenderUtilityHelpers.Instance.EndPreview();
+    }
+
+    /// <summary>
+    /// Renders a thumbnail of the Texture3D
+    /// </summary>
+    /// <param name="texture3D">The Texture3D to preview</param>
+    /// <param name="rect">The area where the preview is located</param>
+    /// <param name="angle">The camera angle</param>
+    /// <param name="distance">The distance of the camera to the preview cube</param>
+    /// <param name="samplingIterations">The amount of slices used to raymarch in the Texture3D</param>
+    /// <param name="density">A linear factor to multiply the Texture3D with</param>
+    /// <returns>A Texture2D with the thumbnail</returns>
+    public static Texture2D RenderTexture3DStaticPreview(this Texture3D texture3D, Rect rect, Vector2 angle, float distance, int samplingIterations, float density)
+    {
+        PreviewRenderUtilityHelpers.Instance.BeginStaticPreview(rect);
+        
+        RenderInPreviewRenderUtility(texture3D, angle, distance, samplingIterations, density);
+
+        return PreviewRenderUtilityHelpers.Instance.EndStaticPreview();
+    }
+    #endregion
+}
---
+++ README.md
@@ -0,0 +1,77 @@
+# UPM version of **[Texture3D](https://docs.unity3d.com/ScriptReference/Texture3D.html) preview for [Unity](https://unity3d.com)**
+
+## Installation
+
+Add something like this to your `Packages/manifest.json`
+"com.texture3d.preview": "git://github.com/alerdenisov/com.texture3d.preview.git",
+
+
+# **[Texture3D](https://docs.unity3d.com/ScriptReference/Texture3D.html) preview for [Unity](https://unity3d.com)**
+
+## This package enables interactive previews of [Texture3D](https://docs.unity3d.com/ScriptReference/Texture3D.html) assets in Unity's Inspector window.
+
+**[You can directly download the Unity package by clicking here or by going in the Release section](https://github.com/raphael-ernaelsten/Texture3DPreview-for-Unity/releases)**
+
+----------
+
+### Previews and thumbnails of [Texture3D](https://docs.unity3d.com/ScriptReference/Texture3D.html) asset
+
+When importing the Texture3D asset, Unity will automatically render a preview of the Texture3D to display in the Project window.
+
+![Texture3D asset thumbnail](https://i.imgur.com/K9IhLF3.jpg)
+
+When selecting a Texture3D asset in the Project windows, Unity will display an preview of the Texture3D in the Inspector.
+
+![Texture3D asset preview](https://i.imgur.com/Lm5Kykw.gif)
+
+----------
+
+### Previews of a [Texture3D](https://docs.unity3d.com/ScriptReference/Texture3D.html) field on a GameObject's component
+
+To enable Texture3D field preview on a GameObject's component, add 
+
+    [Texture3DPreview]
+in front of the declared field.
+
+![Texture3D field preview in Inspector](https://i.imgur.com/ru8u1qK.gif)
+
+----------
+
+### Requirements 
+
+ - Shader model 3 capable graphic card
+ - Unity 2017.1+
+
+----------
+
+### TODO 
+
+ - Preview non uniform Texture3D in their respective ratio (currently all previews will be cube)
+ - Add alpha blend mode for rendering previews
+
+----------
+
+### Know issues / limitations
+
+ - Previews are currently in additive mode
+ - Previews of multiples Texture3D assets don't work
+ - Sometimes, the preview of a Texture3D field becomes empty
+
+----------
+
+### Contact
+
+Feel free to contact me for any comment or suggestion. Twitter : @raphernaelsten
+
+----------
+
+### Acknowledgment
+
+The sample Texture3D asset provided with this package was made using [MRI scans found on this website](https://neil.fraser.name/news/2007/11/19/).
+
+Here's the process :
+ - selected [the right-to-left gif](https://neil.fraser.name/news/2007/RL.gif)
+ - rescaled it to 128x128 and extracted all the frames using [VirtualDub](http://www.virtualdub.org/)
+ - used [Unity's VFX Toolbox Image Sequencer](https://forum.unity.com/threads/release-thread-vfx-toolbox-image-sequencer.438465/) to resample the frames count and lay them on an images sheet
+ ![Images sheet](https://i.imgur.com/hJvhZ78.jpg)
+ - used a custom tool to build a Texture3D (basically create a Texture3D with the correct size then copy the images from the sheet as slices) from this sheet and save it as an asset
